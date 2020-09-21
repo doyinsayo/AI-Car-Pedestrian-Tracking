79ac8c6013a21a26e7c81e621c77019dccc5912c
Doyinsayo Otufadebo <doyinsa01@gmail.com>
2020-09-21 18:05:42 +0100

    'pedestrian-classifier-file

--- CAR-PEDESTRIAN-TRACKING.py
+++ CAR-PEDESTRIAN-TRACKING.py
@@ -2,10 +2,15 @@ import cv2
 
 #Our image
 img_file = 'car_images.jpg'
+#video = cv2.VideoCapture('')
 video = cv2.VideoCapture('')
 
 #Our pre-trained car classifier
 classifier_file = 'car_detector.xml'
+
+# create car classifier
+car_tracker = cv2.CascadeClassifier(cv2.data.haarcascades+classifier_file)
+pedestrian_tracker = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fullbody.xml')
 
 #Run unless interrupted
 while True:
@@ -18,13 +23,32 @@ while True:
         #must convert to grayscale
         grayscaled_frame = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     else:
-        break    
+        break   
+
+    #detect cars
+    cars = car_tracker.detectMultiScale(black_n_white)
+    pedestrians = pedestrian_tracker.detectMultiScale(grayscaled_frame) 
+        
+    # Draw rectangles around the cars
+    for (x,y,w,h) in cars:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,0,255),2)
+
+    # Draw rectangles around the pedestrians
+    for (x,y,w,h) in pedestrians:
+        cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,255),2)    
 
     # Display the image with the cars spotted
     cv2.imshow('Car_detector',grayscaled_frame)
 
     #Don't autoclose
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+
+    # stop if Q key is pressed
+    if key==81 or key==113:
+        break
+
+# release the videocapture object
+video.release()
 
 """
 #create opencv image
